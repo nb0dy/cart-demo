6ae832432ea2fed9e7b1fdadc1cef63e3e60cf7d
marcinludwig <marcinl@eb2b.com.pl>
2018-08-19 21:30:51 +0200

    Add http config

---
+++ src/api.tsx
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const instance = axios.create({
+  baseURL: 'https://5b79c19dfb11c800145362b0.mockapi.io/',
+  responseType: 'json'
+})
+
+instance.interceptors.response.use(response => {
+  if (response.status === 401 || response.status === 403) {
+    return Promise.reject("You don't have the permissions")
+  }
+
+  return response
+}, error => {
+  console.log('error')
+  return Promise.reject(error)
+})
